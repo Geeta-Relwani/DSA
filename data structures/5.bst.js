df7b220fedfaa7583dd63ab3e6f488aba8aca184
Geeta Relwani <geetarelwani@gmail.com>
2024-02-16 10:54:29 +0530

    tree traversal

--- data structures/5.bst.js
+++ data structures/5.bst.js
@@ -60,13 +60,74 @@ class BinarySearchTree{
                 console.log('not found')
             }
         }
+
+        //breadth first search will return node in bfs orders in an array
+        BFS(){
+            var node = this.root
+            var data = []
+            var queue = []
+            queue.push(this.root)
+
+            while(queue.length){
+                node = queue.shift()
+                data.push(node.value)
+                if(node.left) queue.push(node.left)
+                if(node.right) queue.push(node.right)
+            }
+        console.log('data',data)
+        return data
+        }
+
+        DSFPreOrder(){
+            var data = []
+            function traverse(node){
+                data.push(node.value)
+                if(node.left) traverse(node.left)
+                if(node.right) traverse(node.right)
+            }
+            traverse(this.root)
+            console.log('dsf preorder -', data)
+            return data
+        }
+
+        DSFPostOrder(){
+            var data = []
+
+            function traverse(node){
+                if(node.left) traverse(node.left)
+                if(node.right) traverse(node.right)
+                data.push(node.value)
+               
+            }
+            traverse(this.root)
+            console.log('DFS Post Order - ', data)
+            return data
+        }
+
+        DSFinOrder(){
+            var data = []
+
+            function traverse(node){
+                if(node.left) traverse(node.left)
+                data.push(node.value)
+                if(node.right) traverse(node.right)
+               
+            }
+            traverse(this.root)
+            console.log('DFS in Order - ', data)
+            return data
+        }
     
 }
 
 var tree = new BinarySearchTree()
-tree.insert(3)
-tree.insert(1)
-tree.insert(2)
-tree.insert(4)
+tree.insert('C')
+tree.insert('B')
+tree.insert('A')
+tree.insert('D')
 console.log(tree)
 tree.find(11)
+tree.BFS()
+tree.DSFPreOrder()
+tree.DSFPostOrder()
+tree.DSFinOrder()
